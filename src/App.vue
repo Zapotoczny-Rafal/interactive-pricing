1dcfb43334f2a14a7bd205bc7942df249bd18274
Rafał Zapotoczny <zapotoczny169@wp.pl>
2022-02-17 18:47:43 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,209 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="circle-container"></div>
+  <div class="main-container">
+    <h1>Simple, traffic-based pricing</h1>
+    <h2>Sign-up for our 30-day trial. No credit card required.</h2>
+    <CardPricing />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardPricing from './components/CardPricing'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { CardPricing }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@600;800&display=swap');
+
+:root {
+  --clr-black: hsl(227, 35%, 25%);
+  --clr-grey-blue: hsl(225, 20%, 60%);
+  --clr-light-red: hsl(15, 100%, 70%);
+  --clr-pale-blue: hsl(226, 100%, 87%);
 }
+
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  box-sizing: border-box;
+  -webkit-tap-highlight-color: transparent;
+}
+
+html {
+  font-size: 15px;
+}
+
+html, body {
+  width: 100%;
+  height: 100%;
+  background-color: hsl(230, 100%, 99%);
+}
+
+input[type=range] {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  height: 8px;
+  background: linear-gradient(to right, hsl(174, 77%, 80%) 0%,
+                                        hsl(174, 77%, 80%) 50%, 
+                                        hsl(224, 65%, 95%) 50%,
+                                        hsl(224, 65%, 95%) 100%);
+  border-radius: 9px;
+}
+
+#app {
+  font-family: 'Manrope', sans-serif;
+  text-align: center;
+  color: var(--clr-black);
+  width: 100%;
+  height: 100%;
+  background-color: hsl(230, 100%, 99%);
+  background-image: url(assets/images/bg-pattern.svg);
+  background-position: left top;
+  background-size: auto;
+  background-repeat: no-repeat;
+}
+
+.circle-container {
+  z-index: 0;
+  position: absolute;
+  width: 100%;
+  height: 150px;
+  background-image: url(assets/images/pattern-circles.svg);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  top: 64px;
+}
+
+.main-container {
+  position: relative;
+  top: 105px; 
+}
+
+.main-container h1 {
+  font-size: 2rem;
+  font-weight: 800;
+}
+.main-container h2 {
+  font-size: 1rem;
+  font-weight: 600;
+  color: var(--clr-grey-blue);
+  margin-top: 11px;
+}
+
+@media screen and (min-width: 90em) {
+  #app {
+    background-image: url(assets/images/bg-pattern.svg), url(assets/images/bg-pattern.svg), url(assets/images/bg-pattern.svg);
+    background-position: left top, 1250px top, 2500px top;
+  }
+
+  .container-billing .yearly-discount-sm {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 41em) {
+  #app {
+    background-position: left -3em;
+  }
+
+  .container-billing .yearly-discount-lg {
+    display: none;
+  }
+
+  .main-container h1 {
+    font-size: 1.26rem;
+    letter-spacing: 0.5px;
+  }
+
+  .main-container h2 {
+    margin: auto;
+    margin-top: 9px;
+    font-size: 0.87rem;
+    letter-spacing: 0.3px;
+    width: 190px;
+    line-height: 23px;
+  }
+
+  .card-pricing {
+    width: 325px;
+    height: 475px;
+    margin-top: 65px;
+  }
+
+  .card-pricing .container-slider {
+    padding: 0px 23px;
+  }
+
+  .container-views-price {
+    display: flex;
+    justify-content: center;
+  }
+
+  .container-views-price .price {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 95px;
+    width: 100%;
+    font-size: 2.13rem;
+  }
+  
+  .container-views-price .price .time {
+    margin-left: 5px;
+  }
+
+  .price-slider {
+    width: 280px;
+    position: absolute;
+    top: 90px;
+  }
+
+  .container-views-price .page-views {
+    font-size: 0.8rem;
+    top: -15px;
+    left: 0px;
+    position: absolute;
+    width: 100%;
+  }
+
+  .container-billing {
+    padding: 0 5px 0 0;
+    position: absolute;
+    top: 200px;
+  }
+
+  .container-billing > p {
+    margin: 3px;
+  }
+
+  .container-description {
+    flex-direction: column;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .container-description .benefits {
+    flex-direction: row;
+    flex-wrap: wrap;
+    position: relative;
+    top: 15px;
+    right: 20px;
+    width: 200px
+  }
+
+  .container-description .start {
+    position: relative;
+    top: 35px;
+  }
+}
+
 </style>
---
+++ src/components/CardPricing.vue
@@ -0,0 +1,309 @@
+<template>
+  <div class="card-pricing">
+    <div class="container-slider">
+      <div class="container-views-price">
+        <p class="page-views">{{ isPageViews }} pageviews</p>
+        <p class="price">
+          ${{ isPrice }}.00
+          <span class="time">/ {{ isTime }}</span>
+        </p>
+      </div>
+      <input @input="handleStyle($event)"  type="range" name="price-slider" v-model="range" class="price-slider" min="1" max="5" step="1" aria-valuemin="1" aria-valuemax="5" aria-valuenow="3" label="price slider">
+      <div class="container-billing">
+        <p class="monthly-billing">Monthly Billing</p>
+        <label class="switch">
+          <input type="checkbox" class="interval-switch" aria-checked="false" aria-label="yearly payment switch">
+          <span @click="toggleSlider" :class="{active: isYearly}" class="toggle-slider"></span>
+        </label>
+        <p class="yearly-billing">Yearly Billing</p>
+        <p class="yearly-discount-lg">25% discount</p>
+        <p class="yearly-discount-sm">-25%</p>
+      </div>
+    </div>
+    <div class="container-description">
+      <div class="benefits">
+        <p class="benefit">Unlimited websites</p>
+        <p class="benefit">100% data ownership</p>
+        <p class="benefit">Email reports</p>
+      </div>
+      <div class="start">
+        <button class="btn-trial">Start my trial</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isYearly: false,
+      range: 3,
+      prices: {
+        1: {
+          views: '10K',
+          price: 8
+        },
+        2: {
+          views: '50K',
+          price: 12
+        },
+        3: {
+          views: '100K',
+          price: 16
+        },
+        4: {
+          views: '500K',
+          price: 24
+        },
+        5: {
+          views: '1M',
+          price: 36
+        },
+      }
+    }
+  },
+  methods: {
+    toggleSlider() {
+      this.isYearly = !this.isYearly
+    },
+    handleStyle(e) {
+      let range = e.target 
+      let max = range.max
+      let min = range.min
+      let value = range.value
+      const styleVal = (value - min) / (max - min) * 100
+      range.style.background = 'linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ' + styleVal + '%, hsl(224, 65%, 95%) ' + styleVal + '%, hsl(224, 65%, 95%) 100%)'
+    }
+  },
+  computed: {
+    isPrice() {
+      let price = this.prices[this.range].price
+      return this.isYearly ? price * 9 : price
+    },
+    isTime() {
+      return this.isYearly === true ? 'year' :  'month'
+    },
+    isPageViews() {
+      return this.prices[this.range].views
+    }
+  }
+}
+</script>
+
+<style>
+.card-pricing {
+  position: relative;
+  margin: auto;
+  margin-top: 90px;
+  width: 540px;
+  height: 400px;
+  background-color: hsl(0, 0%, 100%);
+  border-radius: 1rem;
+  box-shadow: 0px 15px 15px 7px hsl(224deg, 65%, 95%);
+}
+
+.container-slider {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 260px;
+  padding: 0px 40px;
+}
+
+.container-views-price {
+  width: 100%;
+  height: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  position: absolute;
+  top: 40px;
+}
+
+.container-views-price .page-views {
+  position: relative;
+  font-size: 0.93rem;
+  color: var(--clr-grey-blue);
+  letter-spacing: 0.1rem;
+  text-transform: uppercase;
+  white-space: nowrap;
+  left: -30px;
+}
+
+.container-views-price .price {
+  font-size: 2.6rem;
+  font-weight: 800;
+}
+
+.container-views-price .price .time {
+  font-size: 1rem;
+  color: var(--clr-grey-blue);
+  font-weight: 600;
+}
+
+.price-slider {
+  position: relative;
+  width: 100%;
+  cursor: pointer;
+  top: 15px;
+}
+
+.price-slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 40px;
+  height: 40px;
+  background-image: url(../assets/images/icon-slider.svg);
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-color: hsl(174, 86%, 45%);
+  border-radius: 50%;
+  cursor: pointer;
+  box-shadow: 0px 10px 30px 2px hsl(174, 86%, 45%);
+}
+
+.price-slider::-webkit-slider-thumb:hover {
+  opacity: 0.8;
+  transition: linear 300ms;
+} 
+
+.price-slider::-moz-range-thumb {
+  width: 40px;
+  height: 40px;
+  background-image: url(../assets/images/icon-slider.svg);
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-color: hsl(174, 86%, 45%);
+  border-radius: 50%;
+  cursor: pointer;
+  box-shadow: 0px 15px 15px 7px hsl(174, 86%, 45%);
+}
+
+.price-slider::-moz-range-thumb:hover {
+  opacity: 0.8;
+  transition: linear 300ms; 
+}
+
+.container-billing {
+  position: relative;
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  top: 75px;
+  padding: 0 20px 0 0;
+}
+
+.container-billing > p {
+  margin: 0 8px;
+}
+
+.container-billing .monthly-billing, .container-billing .yearly-billing {
+  color: var(--clr-grey-blue);
+  font-size: 0.86rem;
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 43px;
+  height: 22px;
+}
+
+.switch input {
+  width: 43px;
+  height: 22px;
+}
+
+.toggle-slider {
+  position: absolute;
+  cursor: pointer;
+  inset: 0;
+  background-color: hsl(223, 50%, 87%);
+  transition: background-color 300ms ease-in-out;
+  border-radius: 22px;
+}
+
+.toggle-slider:hover {
+  background-color: hsl(174deg 70% 61%);
+}
+
+.toggle-slider::before {
+  border-radius: 50%;
+  position: absolute;
+  content: "";
+  height: 14px;
+  width: 14px;
+  left: 4px;
+  bottom: 4px;
+  background-color: hsl(0, 0%, 100%);
+  transition: left 300ms ease-out;
+}
+
+.active::before {
+  left: 24px;
+  transition: left 300ms ease-out
+}
+
+.container-billing .yearly-discount-lg, .container-billing .yearly-discount-sm  {
+  background-color: hsl(14, 92%, 95%);
+  color: var(--clr-light-red);
+  font-size: 0.86rem;
+  padding: 3px 5px;
+  border-radius: 20px;
+}
+
+.container-description {
+  display: flex;
+  width: 100%;
+  height: 140px;
+  border-top: 1px solid hsl(224, 65%, 95%);
+}
+
+.container-description .benefits {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 50%;
+  color: hsl(225, 20%, 60%);
+  padding-left: 40px;
+}
+
+.container-description .benefits .benefit {
+  font-size: 0.86rem;
+  padding: 5px;
+  background-image: url(../assets/images/icon-check.svg);
+  background-repeat: no-repeat;
+  background-size: 10px;
+  padding-left: 20px;
+  background-position: left center;
+}
+
+.container-description .start {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+}
+
+.container-description .start .btn-trial {
+  width: 170px;
+  height: 42px;
+  border-radius: 42px;
+  color: hsl(226, 100%, 87%);
+  background-color: hsl(227, 35%, 25%);
+  border: none;
+  cursor: pointer;
+}
+
+.container-description .start .btn-trial:hover {
+  color: hsl(0, 0%, 100%);
+   transition: color 300ms ease-out;
+}
+
+</style>
